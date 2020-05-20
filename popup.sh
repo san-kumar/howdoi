958852b014e0cf087b61c00550dca494b764a317
Your Name <you@example.com>
2020-05-20 08:40:52 +0530

    updated readme

--- popup.sh
+++ popup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Run howdoi in a popup (useful if want to use it outside a terminal via a keybinding)
 
-gnome-terminal -e 'bash -c "howdoi -i; echo Press any key to exit; read line"'
+gnome-terminal --title "How Do I?" -e 'bash -c "howdoi -i; echo Press any key to exit; read line"'
